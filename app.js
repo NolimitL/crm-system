8a0e538721564805c5df2681b264b3166d3b5996
NolimitL <36302876+NolimitL@users.noreply.github.com>
2020-11-15 19:37:32 +0100

    db is connected

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
 const cors = require('cors');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
@@ -8,16 +8,27 @@ const analyticsRoutes = require('./routes/analytics');
 const categoryRoutes = require('./routes/category');
 const orderRoutes = require('./routes/order');
 const positionRoutes = require('./routes/position');
-//middleware
+const DB = require('./config/keys').DB;
+// initialization an app
+const app = express();
+// middlewares
 app.use(cors());
 app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-//routers
+// connecting to Database
+mongoose.connect(DB.MONGO_URL)
+   .then(() => console.log('[Database is successfully connected]'))
+   .catch(err => console.log('[ERROR from DB]: ', err));
+// routers
 app.use('/api/auth', authRoutes);
 app.use('/api/analytics', analyticsRoutes);
 app.use('/api/category', categoryRoutes);
 app.use('/api/order', orderRoutes);
 app.use('/api/position', positionRoutes);
-//exports app
+// exports app
 module.exports = app;
+
+// QVjVjXSpg35dBHYL
+// main-user
+// mongodb+srv://main-user:<password>@cluster0.en0ed.mongodb.net/<dbname>?retryWrites=true&w=majority
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports.DB = {
+   MONGO_URL: 'mongodb+srv://main-user:QVjVjXSpg35dBHYL@cluster0.en0ed.mongodb.net/<dbname>?retryWrites=true&w=majority'
+}
