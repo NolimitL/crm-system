da339e0ad3466c410c91e587698db497006d76b0
NolimitL <36302876+NolimitL@users.noreply.github.com>
2020-11-15 18:57:09 +0100

    Models for DB are created

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/auth');
 const analyticsRoutes = require('./routes/analytics');
@@ -7,6 +9,8 @@ const categoryRoutes = require('./routes/category');
 const orderRoutes = require('./routes/order');
 const positionRoutes = require('./routes/position');
 //middleware
+app.use(cors());
+app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 //routers
@@ -15,5 +19,5 @@ app.use('/api/analytics', analyticsRoutes);
 app.use('/api/category', categoryRoutes);
 app.use('/api/order', orderRoutes);
 app.use('/api/position', positionRoutes);
-
+//exports app
 module.exports = app;
---
+++ models/Orders.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const orderSchema = new Schema({
+   date:{
+      type: Date,
+      default: Date.now
+   },
+   order:{
+      type: Number,
+      required: true,
+
+   },
+   user:{
+      type: Schema.Types.ObjectId,
+      ref: 'users'
+   },
+   list:[
+      {
+         name:{
+            type: String
+         },
+         quantity:{
+            type: Number
+         },
+         cost:{
+            type: Number
+         }
+      }
+   ]
+});
+
+module.exports = mongoose.model('orders', orderSchema);
